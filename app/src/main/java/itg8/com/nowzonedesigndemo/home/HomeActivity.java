d7c7e1c11bc0db1c9a539aff6e7d3a84da9fc428
ayesha <app.itechgalaxy@gmail.com>
2017-06-21 11:33:20 +0530

    Ayesha graph plotting

--- app/src/main/java/itg8/com/nowzonedesigndemo/home/HomeActivity.java
+++ app/src/main/java/itg8/com/nowzonedesigndemo/home/HomeActivity.java
@@ -162,7 +162,7 @@ public class HomeActivity extends BaseActivity implements View.OnClickListener, 
         ButterKnife.bind(this);
         Timber.tag(TAG);
 
-        checkDeviceConnection(rlWave);
+//        checkDeviceConnection(rlWave);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
